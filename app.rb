8b2f371689fafc34459f32553da33a18da8fb5f7
jtorre20 <jtorres1989@gmail.com>
2018-04-03 21:14:52 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do 
+    erb :newteam
+  end
 
+  post '/team' do 
+    @hash = params
+    #puts @hash
+    erb :team
+    
+  end
 end
